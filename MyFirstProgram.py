67fc675457a7328cea32cee677dd0b79c39ae9ff
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-09-10 20:39:38 +0000

    Updated my code

--- MyFirstProgram.py
+++ MyFirstProgram.py
@@ -1,36 +1,58 @@
 print("insert Character")
-Character = input()
-if Character == "Temmie":
+Character = input().lower()
+if Character == "temmie":
     print("hOI!!! im TEMMIE!!")
-if Character == "Sans":
+if Character == "sans":
     print("Do you wanna have a bad time")
-if Character == "Papyrus":
+if Character == "papyrus":
     print("Nyeh heh heh it is I the great papyrus")
-if Character == "Flowey":
+if Character == "flowey":
     print("In this world its kill or be killed")
-if Character == "Undyne":
+if Character == "undyne":
     print("NGAAAAAAAAAA")
-if Character == "Undyne The Undying":
+if Character == "undyne the undying":
     print("Your going to have to try a little harder than that")
-if Character == "W.D. Gaster":
+if Character == "w.d. gaster":
     print("✋ ✌💣 ❄☟☜ 💣✌☠ 🕈☟⚐ 💧🏱☜✌😐💧 ✋☠ ☟✌☠👎💧")
-if Character == "Napstablook":
+if Character == "napstablook":
     print("im sorry im not really feeling up to it right now")
-if Character == "Mettaton":
+if Character == "mettaton":
     print("Hello beauties")
-if Character == "Mettaton EX":
+if Character == "mettaton ex":
     print("Lights, Camera, Bloodshed")
-if Character == "Mettaton Neo":
+if Character == "mettaton neo":
     print("Power of Neo")
-if Character == "Froggit":
+if Character == "froggit":
     print("Meow")
-if Character == "Jevil":
+if Character == "jevil":
     print("I can do anything")
-if Character == "Spamton":
+if Character == "spamton":
     print("Nows your chance to be a big shot")
-if Character == "Asgore":
+if Character == "asgore":
     print("Think of it like a trip to the dentist")
-if Character == "Toriel":
+if Character == "toriel":
     print("I am Toriel, caretaker of the Ruins...")
-if character == "Muffet":
+if Character == "muffet":
     print("I hear your awfully stingy with your money")
+if Character == "chara":
+    print("Let us erase this pointless world, and move on to the next.")
+if Character == "fell!sans":
+    print("were going to have a great time")
+if Character == "dust!sans":
+    print("were going to have a mad time")
+if Character == "sannes":
+    print("now its time to face your bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom bad tom")
+if Character == "fell!papyrus":
+    print("You have ten minutes to get out of here or your skull will be used to serve my spaghetti!!!... was that too mean fine then ill give him twenty minutes")
+if Character == "ink!sans":
+    print("...What were we talking about?")
+if Character == "Toby":
+    print("Im not snoring, Im cheering you on in my sleep!!")
+if Character == "xgaster":
+    print("Evolution requires sacrifice")
+if Character == "cross!sans":
+    print("I'm against unnecessary sacrifices to fix something that is fine as it is.")
+if Character == "fresh!sans":
+    print("get silases help")
+if Character == "geno!sans":
+    print("isolation... is devastating")
