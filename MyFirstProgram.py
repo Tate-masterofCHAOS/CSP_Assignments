d334ebb61ee6fb88fae3fb18ab9b9f18e5eb6ce9
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-09-10 19:44:13 +0000

    Worked on my code

--- MyFirstProgram.py
+++ MyFirstProgram.py
@@ -1,1 +1,36 @@
-
+print("insert Character")
+Character = input()
+if Character == "Temmie":
+    print("hOI!!! im TEMMIE!!")
+if Character == "Sans":
+    print("Do you wanna have a bad time")
+if Character == "Papyrus":
+    print("Nyeh heh heh it is I the great papyrus")
+if Character == "Flowey":
+    print("In this world its kill or be killed")
+if Character == "Undyne":
+    print("NGAAAAAAAAAA")
+if Character == "Undyne The Undying":
+    print("Your going to have to try a little harder than that")
+if Character == "W.D. Gaster":
+    print("✋ ✌💣 ❄☟☜ 💣✌☠ 🕈☟⚐ 💧🏱☜✌😐💧 ✋☠ ☟✌☠👎💧")
+if Character == "Napstablook":
+    print("im sorry im not really feeling up to it right now")
+if Character == "Mettaton":
+    print("Hello beauties")
+if Character == "Mettaton EX":
+    print("Lights, Camera, Bloodshed")
+if Character == "Mettaton Neo":
+    print("Power of Neo")
+if Character == "Froggit":
+    print("Meow")
+if Character == "Jevil":
+    print("I can do anything")
+if Character == "Spamton":
+    print("Nows your chance to be a big shot")
+if Character == "Asgore":
+    print("Think of it like a trip to the dentist")
+if Character == "Toriel":
+    print("I am Toriel, caretaker of the Ruins...")
+if character == "Muffet":
+    print("I hear your awfully stingy with your money")
