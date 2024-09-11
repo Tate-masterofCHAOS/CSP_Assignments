092d2fa83f99d9dcb06788de81cfadf6f1896b76
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-09-11 19:13:47 +0000

    finished variable practice C

---
+++ VariablePractice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    char name = "Tate";
+    char numSmol = "9:";
+    char numBig = "654";
+    char breakfest = "cereal";
+    char col = "green";
+    char sch = "UCAS";
+    char year = "2024";
+    char eyecol = "blue";
+    char age = "14";
+    char favsub = "science math or computer"
+    printf(name);
+    printf(numSmol);
+    printf(numBig);
+    printf(breakfest);
+    printf(col);
+    printf(sch);
+    printf(year);
+    printf(eyecol);
+    printf(age);
+    printf(favsub);
+    return 0;
+}
