86d07eb86a2f2ec83cd5582c304fa08a8c28dc06
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-09-18 04:12:18 +0000

    did the budget for python

--- expressions/budget.py
+++ expressions/budget.py
@@ -1,10 +1,25 @@
-def income():
-def rent():
-def utilities():
-def groceries():
-def transportation():
-def savings():
-def expenses():
-def spend():
-print("This is a budget calculator. How much do you make a month?");
-income = input()
+income = float(input("This is a budget calculator. How much do you make a month?\n"))
+rent = float(input("How much does your rent cost?\n"))
+utilities = float(input("How much does your utilities cost?\n"))
+groceries = float(input("How much do your groceries cost?\n"))
+transportation = float(input("How much does your transportation cost?\n"))
+
+expenses = rent + utilities + groceries + transportation
+savings = income * 0.2
+spend = income - expenses - savings
+
+print(f"You make {income:.2f}")
+print(f"Your expenses are {expenses:.2f}")
+print(f"Your savings are {savings:.2f}")
+print(f"Your spending money is {spend:.2f}")
+
+prent = (rent / income) * 100
+putilities = (utilities / income) * 100
+ptransportation = (transportation / income) * 100
+psavings = (savings / income) * 100
+pexpenses = (expenses / income) * 100
+
+print(f"Your rent is {int(prent)}% of your income.")
+print(f"Your utilities is {int(putilities)}% of your income.")
+print(f"Your transportation is {int(ptransportation)}% of your income.")
+
