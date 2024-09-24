b93873f4e93f726f269c8413a20df98e2721e711
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-09-24 19:25:54 +0000

    did hello world

---
+++ Functions/HelloWorld.py
@@ -0,0 +1,8 @@
+def hello(name):
+    return f"hello{name}";
+
+print(hello(" maria"));
+print(hello(" jonathan"));
+print(hello(" bob the naked mole rat"));
+print(hello(" jared"));
+print(hello(" bread bro"));
---
+++ Functions/helloWorldpractice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void name(name){
+    char name[10];
+    return
+}
+
+int main(void){
+    name;
+    name;
+    name;
+    name;
+    name;
+    return 0;
+}
+
