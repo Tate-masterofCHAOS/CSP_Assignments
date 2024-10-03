ac47ee7cabd9b63131238da9e3eaf51d112432ee
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-10-03 19:09:30 +0000

    finished time

--- Conditional/time.py
+++ Conditional/time.py
@@ -1,7 +1,13 @@
 import datetime
 
 current_time = datetime.datetime.now()
-
 print(current_time.hour)
+time_input = input("Please input time of day in military time: ")
+time = int(time_input)
 
-if current_time.hour
+if time < 12:
+    print("Good Morning")
+elif time < 18:
+    print("Good Afternoon")
+else:
+    print("Good Evening")
