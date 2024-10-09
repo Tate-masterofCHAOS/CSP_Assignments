9dfc57dd0d55f63d7af643ac438ec2bd82eb8876
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-10-09 19:39:13 +0000

    did my family

---
+++ Loops/My_Family.c
@@ -0,0 +1,6 @@
+int i;
+char My_Household[] = {"Tate", "Owen", "Kassandra", "Charlie", "Lady Belle", "Roland", "Bruno", "Thing 1", "Thing 2"};
+while (i=0;i<9;i++){
+    printf("Hello", My_Household[i]);
+    i++;
+}
---
+++ Loops/My_Family.py
@@ -0,0 +1,3 @@
+My_household = ["Tate", "Owen", "Kassandra", "Charlie", "Lady Belle", "Roland", "Bruno", "Thing 1", "Thing 2"]
+for Guy in My_household:
+    print((Guy), "will come for you");
