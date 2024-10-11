1d834c5072cd716abfa22226c4639c091c10657a
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2024-10-11 19:04:18 +0000

    finished my family

--- Loops/My_Family.py
+++ Loops/My_Family.py
@@ -1,3 +1,15 @@
-My_household = ["Tate", "Owen", "Kassandra", "Charlie", "Lady Belle", "Roland", "Bruno", "Thing 1", "Thing 2"]
-for Guy in My_household:
-    print((Guy), "will come for you");
+usr = int(input("Give me a number: "))
+one = input("Give me a short word: \n")
+two = input("Give me another short word: \n")
+three = one + two
+
+for i in range(usr + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print(three)
+    elif i % 3 == 0:
+        print(one)
+    elif i % 5 == 0:
+        print(two)
+    else:
+        print(i)
+
